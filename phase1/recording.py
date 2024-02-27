76be0dcafab4d227a0317408376bed9ec448fd9a
ZixuanShen <127663957+moseicshen@users.noreply.github.com>
2024-02-27 18:50:57 +0800

    Audio2Text Refined

--- phase1/recording.py
+++ phase1/recording.py
@@ -1,5 +1,5 @@
 class Recording():
-    def __init__(self, name, createTime, length, label, path):
+    def __init__(self, name=None, createTime=None, length=0, label=None, path=None):
         self.name = name
         self.createTime = createTime
         self.length = length
