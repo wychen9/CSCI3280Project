2d5285208c176acc32cfba1e37164335451787b7
Foolllll <62875591+Foolllll-J@users.noreply.github.com>
2024-02-28 22:06:46 +0800

    Update player_control.py
    try setting an end time

--- phase1/player_control.py
+++ phase1/player_control.py
@@ -1,24 +1,29 @@
 from soundplayer import AudioPlayer
 
+
 def control(command):
-    global player, start_percentage, set_flag
+    global player, start_percentage, stop_percentage, set_flag
     words = command.split(' ')
     if words[0] == 'load':
         path = words[1]
         player = AudioPlayer(path)
         start_percentage = 0
+        stop_percentage = 100.0
         set_flag=False
-        #total_length = player.get_total_length()
     elif words[0] == 'set':
         start_percentage = float(words[1])
+        if len(words) > 2:
+            stop_percentage = float(words[2])
+        else:
+            stop_percentage = 100.0
         if player.play_obj is None or player.play_obj.is_playing() == False:
             set_flag=True
         else:
-            player.play(start_percentage)
+            player.play(start_percentage, stop_percentage)
     elif words[0] == 'play':
         if set_flag:
             set_flag=False
-            player.play(start_percentage)
+            player.play(start_percentage, stop_percentage)
         else:
             player.play()
     elif words[0] == 'speed':
@@ -29,7 +34,7 @@ def control(command):
     elif command == 'resume':
         if set_flag:
             set_flag=False
-            player.play(start_percentage)
+            player.play(start_percentage, stop_percentage)
         else:
             player.resume()
     elif command == 'stop':
