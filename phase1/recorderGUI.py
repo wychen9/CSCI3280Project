c1384ffe59f38524443c0385eeddc73afd8c46bb
wychen9 <1155141421@link.cuhk.edu.hk>
2024-02-28 22:01:05 +0800

    Update recorderGUI.py

--- phase1/recorderGUI.py
+++ phase1/recorderGUI.py
@@ -246,7 +246,7 @@ def visual():
     if tInt.get() == 1:
         print("visualizing audio")
         ## Sample usage
-        # vis = Visualization(wavRecording, visualFrame)
+        # vis = Visualization(wavRecording, visualFrame) # need to create a new object first with Recording object and the target frame in GUI
         # vis.begin(startSecond, endSecond) # call this methond when need animated visualization from startSecond(by default 0) to endSecond(by default end of the audio)
         # vis.locAt(currentLoc) # call this method when need static visualization, with current location at currentLoc(by default 0)
     return
