39eff4b8084a3e47c5cde757ef1101f258d3157e
wychen9 <1155141421@link.cuhk.edu.hk>
2024-04-02 16:33:44 +0800

    Create room_server.py

---
+++ phase2/room_server.py
@@ -0,0 +1,17 @@
+from Room import Room
+
+class Room_Server(Room):
+    def __init__(self):
+        pass
+    def create_room(self):
+        # Develop a function that allows users to create a new chat room.
+        # This function should enable other computers on the same network to discover and access the created chat room
+        pass
+    def get_room_list(self):
+        # Implement a 'chat room list' feature that displays all the created chat
+        # rooms within the network. This list will help users identify and select the chat room they wish to join
+        pass
+    def join_room(self):
+        # Implement a 'chat room list' feature that displays all the created chat
+        # rooms within the network. This list will help users identify and select the chat room they wish to join.
+        pass
