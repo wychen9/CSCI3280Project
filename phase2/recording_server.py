70fc6dd580ef62a565b9fd002c8e0a714b3bfd89
Berylmys <73904450+berylmys@users.noreply.github.com>
2024-04-08 15:08:27 +0800

    Add files via upload

---
+++ phase2/recording_server.py
@@ -0,0 +1,19 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8888
+DIRECTORY = "shared_recordings"
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+  def __int__(self, *args, **kwargs):
+    super().__init__(*args, directory=DIRECTORY, **kwargs)
+  
+# make sure that shared directory exist
+if not os.path.exists(DIRECTORY):
+  os.makedirs(DIRECTORY)
+
+# set HTTP server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+  print(f"Server started at localhost:{PORT}")
+  httpd.serve_forever()
