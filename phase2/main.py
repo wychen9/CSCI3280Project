40b4d160af7613481e3bb3b21aa4c8d16a9989cc
ZixuanShen <127663957+moseicshen@users.noreply.github.com>
2024-04-11 19:58:05 +0800

    Update main.py

--- phase2/main.py
+++ phase2/main.py
@@ -8,12 +8,15 @@ import sys
 
 ind = int(sys.argv[1])
 ip = "127.0.0.1"
-if(len(sys.argv)>2): ip = sys.argv[2] # need ip inpu if available
+if(len(sys.argv)>2): 
+    ip = sys.argv[2]
+    ip2 = sys.argv[3]
+    # need ip inpu if available
 
 m1 = memberList[ind]
 c1 = Room_Client(m1, ip)
 # need to know the ip address of the server
-audio_client.control("start 127.0.0.1 9808")
+audio_client.control("start "+ ip2 +" 9808")
 eventHandler = EventHandler.EventHandler()
 homepageGUI = HomepageGUI.HomepageGUI(eventHandler, c1)
 eventHandler.setHomepageGUI(homepageGUI)
