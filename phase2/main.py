d8720fa1c93bf8beb37d54b0bc910fbfb8724f93
ZixuanShen <127663957+moseicshen@users.noreply.github.com>
2024-04-11 21:56:16 +0800

    Update main.py

--- phase2/main.py
+++ phase2/main.py
@@ -1,8 +1,6 @@
 import HomepageGUI
 import EventHandler
 import audio_client
-from recording_server import RecordingServer
-from recording_client import RecordingClient
 from room_client import Room_Client
 from Member import memberList
 
@@ -14,13 +12,11 @@ if(len(sys.argv)>2):
     ip = sys.argv[2]
     ip2 = sys.argv[3]
     # need ip inpu if available
-recording_server = RecordingServer()
 
 m1 = memberList[ind]
 c1 = Room_Client(m1, ip)
 # need to know the ip address of the server
 audio_client.control("start "+ ip2 +" 9808")
-recording_client = RecordingClient(c1.get_room_name())
 eventHandler = EventHandler.EventHandler()
 homepageGUI = HomepageGUI.HomepageGUI(eventHandler, c1)
 eventHandler.setHomepageGUI(homepageGUI)
